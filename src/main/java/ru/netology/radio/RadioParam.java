5a07f0020c82d014ff665e4596eb897a1959e685
Anastasia Shusmskaya <anastasiakoa@icloud.com>
2021-03-21 17:30:08 +0300

    2

--- src/main/java/ru/netology/radio/RadioParam.java
+++ src/main/java/ru/netology/radio/RadioParam.java
@@ -2,51 +2,68 @@ package ru.netology.radio;
 
 public class RadioParam {
 
-    private final static int FIRST_RADIO_WAVE = 0;
-    private final static int LAST_RADIO_WAVE = 9;
+    public final static int FIRST_RADIO_WAVE = 0;
+    public final static int LAST_RADIO_WAVE = 9;
 
-    private final static int MAX_VOLUME = 10;
-    private final static int MIN_VOLUME = 0;
+    public final static int MAX_VOLUME = 10;
+    public final static int MIN_VOLUME = 0;
 
-    private int currentRadioWave;
-    private int currentVolume;
+    private int currentRadioWave = FIRST_RADIO_WAVE;
+    private int currentVolume = MIN_VOLUME;
 
+    public RadioParam() {
+
+    }
+
+
+    RadioParam(int currentRadioWave, int currentVolume) {
+        this.currentRadioWave = currentRadioWave;
+        this.currentVolume = currentVolume;
+    }
 
     public int getCurrentRadioWave() {
         return currentRadioWave;
     }
 
-    public void setCurrentRadioWave(int currentRadioWave) {
-        if (currentRadioWave >= FIRST_RADIO_WAVE) {
-            if (currentRadioWave <= LAST_RADIO_WAVE) {
-                this.currentRadioWave = currentRadioWave; //при значении текущей станции между 0 и 9
-            }
-        }
-        if (currentRadioWave > LAST_RADIO_WAVE) {
-            this.currentRadioWave = FIRST_RADIO_WAVE; //при переключении следующей волны за 9ой станцией
-        }
+    public void setCurrentRadioWave(int currentRadioWave) { //выбор радиостанции с пульта
         if (currentRadioWave < FIRST_RADIO_WAVE) {
-            this.currentRadioWave = LAST_RADIO_WAVE; //при переключении предыдущей волны перед 0ой станцией
+            this.currentRadioWave = LAST_RADIO_WAVE;
+        } else if (currentRadioWave > LAST_RADIO_WAVE) {
+            this.currentRadioWave = FIRST_RADIO_WAVE;
+        } else {
+            this.currentRadioWave = currentRadioWave;
         }
     }
 
+    public void chanelUp() {  //переключение на одну радиостанцию
+        if (currentRadioWave < LAST_RADIO_WAVE) {
+            currentRadioWave++;
+        } else {
+            currentRadioWave = FIRST_RADIO_WAVE;
+        } //при переключении следующей волны за 9ой станцией кнопкой +
+    }
+
+    public void chanelDown() {
+        if (currentRadioWave > FIRST_RADIO_WAVE) {
+            currentRadioWave--;
+        } else {
+            currentRadioWave = LAST_RADIO_WAVE;
+        }  //при переключении предыдущей волны перед 0ой станцией кнопкой -
+    }
 
     public int getCurrentVolume() {
         return currentVolume;
     }
 
-    public void setCurrentVolume(int currentVolume) {
-        if (currentVolume >= MIN_VOLUME) {
-            if (currentVolume <= MAX_VOLUME) {
-                this.currentVolume = currentVolume; //при значении текущей громкости между 0 и 10
-            }
+    public void volumeUp() { //увеличение громкости
+        if (currentVolume < MAX_VOLUME) {
+            currentVolume++;
         }
-        if (currentVolume > MAX_VOLUME) {
-            this.currentVolume = MAX_VOLUME; //при переключении громкости выше максимальной
-        }
-        if (currentVolume < MIN_VOLUME) {
-            this.currentVolume = MIN_VOLUME; //при переключении громкости ниже минимальной
+    }
 
+    public void volumeDown() { //уменьшение громкости
+        if (currentVolume > MIN_VOLUME) {
+            currentVolume--;
         }
     }
 
--- src/test/java/ru/netology/radio/RadioParamTest.java
+++ src/test/java/ru/netology/radio/RadioParamTest.java
@@ -10,7 +10,7 @@ class RadioParamTest {
 
     @ParameterizedTest
     @ValueSource(ints = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9})
-    public void radioWaveInLimit(int wave) {       //станция в пределе от 0 до 9
+    public void radioWaveInLimit(int wave) {       // переключение станции с пульта в диапазоне от 0 до 9
         RadioParam radioParam = new RadioParam();
         radioParam.setCurrentRadioWave(wave);
 
@@ -20,52 +20,98 @@ class RadioParamTest {
     }
 
     @Test
-    public void radioWaveOverLimit() {       //переключение станции с 9 на 0
+    public void radioWaveOverLimit() {       // ввод 10ой станции с пульта
         RadioParam radioParam = new RadioParam();
         int newWave = 10;
-        int expected = 0;
+        int expected = RadioParam.FIRST_RADIO_WAVE;
 
         radioParam.setCurrentRadioWave(newWave);
         assertEquals(expected, radioParam.getCurrentRadioWave());
     }
 
     @Test
-    public void radioWaveUnderLimit() {       //переключение станции с 0 на 9
+    public void radioWaveUnderLimit() {       // ввод -10й станции с пульта
         RadioParam radioParam = new RadioParam();
-        int expected = 9;
+        int expected = RadioParam.LAST_RADIO_WAVE;
         int newWave = -1;
 
         radioParam.setCurrentRadioWave(newWave);
         assertEquals(expected, radioParam.getCurrentRadioWave());
     }
 
-    @ParameterizedTest
-    @ValueSource(ints = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10})
-    public void volumeInLimit(int volume) {       //переключение звука от 0 на 10
+    @Test
+    public void waveInLimit() {       //переключение станции кнопкой от 0 до 9
         RadioParam radioParam = new RadioParam();
 
+        for (int i = RadioParam.FIRST_RADIO_WAVE; i <= RadioParam.LAST_RADIO_WAVE; i++) {
+            assertEquals(i, radioParam.getCurrentRadioWave());
+            radioParam.chanelUp();
+        }
+    }
 
-        radioParam.setCurrentVolume(volume);
-        assertEquals(volume, radioParam.getCurrentVolume());
+    @Test
+    public void waveInLimitReverse() {       // переключение станции кнопкой с 9 до 0
+        RadioParam radioParam = new RadioParam(RadioParam.LAST_RADIO_WAVE, RadioParam.MIN_VOLUME);
+
+        for (int i = RadioParam.LAST_RADIO_WAVE; i >= RadioParam.FIRST_RADIO_WAVE; i--) {
+            assertEquals(i, radioParam.getCurrentRadioWave());
+            radioParam.chanelDown();
+        }
+    }
+
+    @Test
+    public void waveOverLimit() {       //переключение станции кнопкой с 9 на 10
+        RadioParam radioParam = new RadioParam(RadioParam.LAST_RADIO_WAVE, RadioParam.MIN_VOLUME);
+
+        radioParam.chanelUp();
+        int expected = RadioParam.FIRST_RADIO_WAVE;
+        assertEquals(expected, radioParam.getCurrentRadioWave());
+    }
+
+    @Test
+    public void waveUnderLimit() {       //переключение станции кнопкой с 0 на -1
+        RadioParam radioParam = new RadioParam();
+
+        radioParam.chanelDown();
+        int expected = RadioParam.LAST_RADIO_WAVE;
+        assertEquals(expected, radioParam.getCurrentRadioWave());
+    }
+
+    @Test
+    public void volumeInLimit() {       // переключение звука от 0 на 10
+        RadioParam radioParam = new RadioParam();
+
+        for (int i = RadioParam.MIN_VOLUME; i <= RadioParam.MAX_VOLUME; i++) {
+            assertEquals(i, radioParam.getCurrentVolume());
+            radioParam.volumeUp();
+        }
+    }
+
+    @Test
+    public void volumeInLimitReverse() {       // переключение звука с 10 до 0
+        RadioParam radioParam = new RadioParam(RadioParam.FIRST_RADIO_WAVE, RadioParam.MAX_VOLUME);
+
+        for (int i = RadioParam.MAX_VOLUME; i >= RadioParam.MIN_VOLUME; i--) {
+            assertEquals(i, radioParam.getCurrentVolume());
+            radioParam.volumeDown();
+        }
     }
 
     @Test
     public void volumeOverLimit() {       //переключение звука с 10 на 11
-        RadioParam radioParam = new RadioParam();
-        int expected = 10;
-        int newVolume = 11;
+        RadioParam radioParam = new RadioParam(RadioParam.MIN_VOLUME, RadioParam.MAX_VOLUME);
 
-        radioParam.setCurrentVolume(newVolume);
+        radioParam.volumeUp();
+        int expected = RadioParam.MAX_VOLUME;
         assertEquals(expected, radioParam.getCurrentVolume());
     }
 
     @Test
     public void volumeUnderLimit() {       //переключение звука с 0 на -1
         RadioParam radioParam = new RadioParam();
-        int expected = 0;
-        int newVolume = -1;
 
-        radioParam.setCurrentVolume(newVolume);
+        radioParam.volumeDown();
+        int expected = RadioParam.MIN_VOLUME;
         assertEquals(expected, radioParam.getCurrentVolume());
     }
 }
